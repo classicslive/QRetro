33d517075d6a08abbbf4898d55095880c75bcdbd
celerizer <celerizer@gmail.com>
2026-03-14 11:48:50 -0500

    shutup warning

--- QRetroEnvironment.h
+++ QRetroEnvironment.h
@@ -1,12 +1,12 @@
 #ifndef QRETRO_ENVIRONMENT_H
 #define QRETRO_ENVIRONMENT_H
 
+#include <stddef.h>
 #include <stdint.h>
 
 bool core_environment(unsigned cmd, void *data);
 
-void core_video_refresh(const void *data, unsigned width, unsigned height,
-                        size_t pitch);
+void core_video_refresh(const void *data, unsigned width, unsigned height, size_t pitch);
 
 void core_audio_sample(int16_t left, int16_t right);
 
